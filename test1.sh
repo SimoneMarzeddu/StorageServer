0a327b52dc93e132aa126779af8f0748b23262ff
SimoneMarzeddu <75311229+SimoneMarzeddu@users.noreply.github.com>
2021-08-26 00:13:27 +0200

    UPDATE
    bug fix for test-1

--- test1.sh
+++ test1.sh
@@ -7,11 +7,12 @@ spid=$! #pid del processo più recente
 
 # fine parte makefile
 
-sleep 2 #attesa post avvio server
+echo "ATTESA : Avvio del Server"
+sleep 5 #attesa post avvio server
 
 ./client -h  #l'opzione -h farà terminare il client, la testo separatamente
 
-./client -f ./ssocket.sk -t 200 -w ./Test1/cartella1/sottocartella1 -W ./Test/cartella1/f1.txt -D ./Test1/cartella1/w_aux_dir1 -r f4,f5 -d ./Test1/cartella1/r_aux_dir1 -R -l f4 -u f4 -c f5 -p
+./client -f ./ssocket.sk -t 200 -w ./Test1/cartella1/sottocartella1 -W ./Test1/cartella1/f1.txt -D ./Test1/cartella1/w_aux_dir1 -r ./Test1/cartella1/sottocartella1/f4.txt,./Test1/cartella1/sottocartella1/f5.txt -d ./Test1/cartella1/r_aux_dir1 -R -l f4 -u f4 -c f5 -p
 #invio di sighup al server
 
 kill -s SIGHUP $spid
